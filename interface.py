93dc16b9bb6779898142fbfc6d7b3cb92c6aa6b5
yungsincheng <37551374+yungsincheng@users.noreply.github.com>
2025-12-20 07:33:08 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -2438,8 +2438,8 @@ else:
                 # ===== 亂碼情況：提取失敗（置中）=====
                 st.markdown(f'''
                 <div style="text-align: center;">
-                    <p style="font-size: 40px; font-weight: bold; color: #C62828; margin-bottom: 30px;">提取失敗 ({r["elapsed_time"]:.2f} 秒)</p>
-                    <p style="font-size: 30px; font-weight: bold; color: #C62828; margin-bottom: 10px;">機密圖像:</p>
+                    <div style="font-size: 40px; font-weight: bold; color: #C62828; margin-bottom: 30px;">提取失敗 ({r["elapsed_time"]:.2f} 秒)</p>
+                    <div style="font-size: 30px; font-weight: bold; color: #C62828; margin-bottom: 10px;">機密圖像:</p>
                 </div>
                 ''', unsafe_allow_html=True)
                 _, img_col, _ = st.columns([1.3, 0.6, 1.2])
